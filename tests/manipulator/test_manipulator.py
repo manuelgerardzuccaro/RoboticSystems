05471ecb94f29ca24a5b11f76596b9d2805c770a
corrado <santoro@dmi.unict.it>
2022-04-19 23:07:00 +0200

    added planar manipulator

---
+++ tests/manipulator/test_manipulator.py
@@ -0,0 +1,41 @@
+#
+# test_manipulator.py
+#
+
+import sys
+sys.path.insert(0, '../../lib')
+
+from models.manipulator import *
+from models.robot import *
+from gui.three_joints_gui import *
+
+from PyQt5.QtWidgets import QApplication
+
+class ManipulatorRobot(RoboticSystem):
+
+    def __init__(self):
+        super().__init__(1e-3) # delta_t = 1e-3
+        self.arm = ThreeJointsPlanarArm(0.2, 0.2, 0.02,
+                                        0.5, 0.5, 0.5,
+                                        0.8)
+
+    def run(self):
+        t1 = 4.0
+        t2 = 5.0
+        t3 = 1.0
+        self.arm.evaluate(self.delta_t, t1, t2, t3)
+        return True
+
+    def get_pose(self):
+        return self.arm.get_pose()
+
+    def get_pose_degrees(self):
+        return self.arm.get_pose_degrees()
+
+
+
+if __name__ == '__main__':
+    robot = ManipulatorRobot()
+    app = QApplication(sys.argv)
+    ex = ManipulatorWindow(robot)
+    sys.exit(app.exec_())
