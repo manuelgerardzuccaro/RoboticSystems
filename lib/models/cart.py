92601cfd3c5f0ed7162dab19be023d1234cba947
LightDestory <apb231@gmail.com>
2023-03-28 15:50:40 +0200

    chore: Some typing and docstrings

--- lib/models/cart.py
+++ lib/models/cart.py
@@ -1,23 +1,23 @@
 class Cart:
-    def __init__(self, _mass, _friction):
+    def __init__(self, _mass: float, _friction: float):
         """
         Defines a cart robot of a 1D environment with the given mass and friction
         :param _mass: The mass of the cart, expressed in Kg
         :param _friction: The force of friction present in the system
         """
-        self.M = _mass
-        self.b = _friction
-        self.speed = 0
-        self.position = 0
+        self.M: float = _mass
+        self.b: float = _friction
+        self.speed: float = 0
+        self.position: float = 0
 
-    def evaluate(self, delta_t, _force):
+    def evaluate(self, delta_t: float, _force: float) -> None:
         """
         Evaluates the linear speed and position at the given time with the applied force
         :param delta_t: The delta time
         :param _force: The applied force
         """
-        new_speed = (1 - self.b * delta_t / self.M) * self.speed + delta_t * _force / self.M
-        new_position = self.position + self.speed * delta_t
+        new_speed: float = (1 - self.b * delta_t / self.M) * self.speed + delta_t * _force / self.M
+        new_position: float = self.position + self.speed * delta_t
         self.speed = new_speed
         self.position = new_position
 
